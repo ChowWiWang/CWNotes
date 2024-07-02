6882bceb657717983bed2d2a864fc144fc72207d
Tim <tim@chowwi.wang>
2024-07-02 13:08:22 -0500

    Update note.sh
    Add a comment describing script

--- note.sh
+++ note.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+#This is a simple notes app. It keeps individual files of each day.
 
 # Create the 'notes' folder in the home directory if it doesn't exist
 mkdir -p "$HOME/notes"
@@ -11,11 +13,10 @@ notes_file="$HOME/notes/$current_date.txt"
 
 # Append notes input to the file when the 'note' command is entered
 while read -p "Enter a note (or type 'exit' to stop): " input; do
-    if [[ "$input" == "exit" ]]; then
-        break
-    fi
-    echo "$input" >> "$notes_file"
+  if [[ "$input" == "exit" ]]; then
+    break
+  fi
+  echo "$input" >>"$notes_file"
 done
 
 echo "Notes saved in $notes_file"
-
